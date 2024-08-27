dc90efb775b30387cedd69877397f97802cbb54c
Danny O'Sullivan <do315@ic.ac.uk>
2024-08-27 23:09:05 +0100

    cleaner implementation

--- main.cpp
+++ main.cpp
@@ -11,11 +11,6 @@ float gen_rand_no(float lb, float ub){
 return lb + (float) rand() / ( (float) (RAND_MAX/(ub - lb)) );
 }
 
-float sArea(float a_x, float a_y, float b_x,float b_y, float c_x, float c_y){
-    return 0.5*(a_x*(b_y - c_y) - a_y*(b_x - c_x) + (b_x*c_y - c_x*b_y));
-}
-
-
 float cart2bary_lamda1(float a_x, float a_y, float b_x,float b_y, float c_x, float c_y, float p_x, float p_y){
     return ((b_y - c_y)*(p_x-c_x) + (c_x - b_x)*(p_y - c_y))/((b_y - c_y)*(a_x - c_x) + (c_x - b_x)*(a_y - c_y));
 }
@@ -24,9 +19,6 @@ float cart2bary_lamda2(float a_x, float a_y, float b_x,float b_y, float c_x, flo
     return ((c_y - a_y)*(p_x - c_x) + (a_x - c_x)*(p_y - c_y))/((b_y - c_y)*(a_x - c_x) + (c_x - b_x)*(a_y - c_y));
 }
 
-
-
-
 float get_x(float r, float theta){
     return r*cos(theta);
 }
@@ -34,34 +26,23 @@ float get_y(float r, float theta){
     return r*sin(theta);
 }
 
-
-
-int check_if_stands(float p[], int table){
+int check_if_stands(float p[]){
     float r1,t1,r2,t2,r3,t3;
-    float sTotalArea, sP23, s1P3, s12P,Lam1, Lam2, Lam3;
+    float Lam1, Lam2, Lam3;
     int res = 0;
 
-    r1 = p[table];
-    t1 = p[table+1];
+    r1 = p[0];
+    t1 = p[1];
 
-    r2 = p[table+2];
-    t2 = p[table+3];
+    r2 = p[2];
+    t2 = p[3];
 
-    r3 = p[table+4];
-    t3 = p[table+5];
-    // cout << r1 << "," << t1 << "::" << r2<< ","<< t2 << "::" << r3 << "," << t3 << endl;
-    // cout << get_x(r1,t1) << "," << get_y(r1,t1) << "::" << get_x(r2,t2) << "," << get_y(r2,t2) << "::" << get_x(r3,t3) << "," << get_y(r3,t3) << endl;
+    r3 = p[4];
+    t3 = p[5];
+   
     Lam1 = cart2bary_lamda1(get_x(r1,t1),get_y(r1,t1),get_x(r2,t2),get_y(r2,t2),get_x(r3,t3),get_y(r3,t3),0.0,0.0);
     Lam2 = cart2bary_lamda2(get_x(r1,t1),get_y(r1,t1),get_x(r2,t2),get_y(r2,t2),get_x(r3,t3),get_y(r3,t3),0.0,0.0);
     Lam3 = 1. - Lam1 - Lam2;
-    // cout << Lam1 << "::" << Lam2 << "::" << Lam3 << endl;
-
-    // sTotalArea = sArea(get_x(r1,t1),get_y(r1,t1),get_x(r2,t2),get_y(r2,t2),get_x(r3,t3),get_y(r3,t3));
-    // sP23 = sArea(0.0,0.0,get_x(r2,t2),get_y(r2,t2),get_x(r3,t3),get_y(r3,t3));
-    // s1P3 = sArea(get_x(r1,t1),get_y(r1,t1),0.0,0.0,get_x(r3,t3),get_y(r3,t3));
-    // s12P = sArea(get_x(r1,t1),get_y(r1,t1),get_x(r2,t2),get_y(r2,t2),0.0,0.0);
-
-    // cout << sTotalArea << ":: "<< sP23 << ":: "<< s1P3 << ":: " << s12P << endl;
     
     if (Lam1 < 0.0){
         res = 0;
@@ -75,34 +56,41 @@ int check_if_stands(float p[], int table){
     else{
         res = 1;
     }
-
     return res;
 }
 
 int main(){
-    int no_tables = 100000;
-    int no_points = 3*no_tables;
+    int no_tables = 1000;
+    int legs_per_table = 3;    
+    float legs[6]; // [(r,theta), (r,theta), (r,theta)]
+
     int no_can_stand = 0;
-    
-    float coords[2*no_points]; // r, theta for unit circle case, flat array (r,theta)i -> (2*i,2*i+1)
 
     float r_lb = 0.;
     float r_ub = 1.;
 
     float theta_lb = 0;
     float theta_ub = 2.*PI;
+//-----------------------------------------------
+
 
     srand(time(0)); // initial random seed
 
-    for(int i=0; i<no_points; i++){
-        coords[2*i]   = gen_rand_no(r_lb,r_ub);
-        coords[2*i+1] = gen_rand_no(theta_lb, theta_ub);
-    }
 
-    for(int i = 0; i < 2*no_points; i=i+6){
-        no_can_stand += check_if_stands(coords,i);
-    }
 
+    for(int t=0; t< no_tables; t++){
+
+        // gen table with 3 random legs 
+        for(int i=0; i< legs_per_table; i++){
+            legs[2*i]   = gen_rand_no(r_lb,r_ub);        // random sampling (r,theta) on disk
+            legs[2*i+1] = gen_rand_no(theta_lb, theta_ub);
+        }
+
+        no_can_stand += check_if_stands(legs);
+
+    }
+    
+    cout << "prob of table standing =" << endl;
     cout << 100 * (float) no_can_stand/ (float) no_tables << "%" <<  endl;
     
     return 0;
